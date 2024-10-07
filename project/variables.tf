735c8604cac147d1bc1ed8be1695b4cacb2f01a4
Your Name <you@example.com>
2024-10-07 04:20:40 +0300

    token auth

--- project/variables.tf
+++ project/variables.tf
@@ -10,3 +10,19 @@ variable "user_data_file" {
   description = "User-data для instance"
   default = "../user-data.txt"
 }
+
+
+variable "yc_token" {
+  description = "OAuth токен для доступа к Yandex Cloud"
+  type        = string
+}
+
+variable "yc_cloud_id" {
+  description = "ID облака в Yandex Cloud"
+  type        = string
+}
+
+variable "yc_folder_id" {
+  description = "ID папки в Yandex Cloud"
+  type        = string
+}
