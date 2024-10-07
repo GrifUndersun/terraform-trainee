cd027796483a10670990ab473b9241745d476422
Your Name <you@example.com>
2024-10-07 08:57:44 +0300

    sg done

--- project/modules/security-groups/main.tf
+++ project/modules/security-groups/main.tf
@@ -9,17 +9,17 @@ terraform {
 resource "yandex_vpc_default_security_group" "default-sg" {
   # name = "bastion-sg"
   network_id = var.network_id
-
-  ingress {
-    protocol        = "ANY"
-    v4_cidr_blocks  = ["0.0.0.0/0"]
-  }
   
   ingress {
     description = var.ssh_rule_name
     protocol    = "TCP"
     port        = 22
     v4_cidr_blocks = [var.ssh_ip]
+  }
+
+  ingress {
+    protocol        = "ANY"
+    v4_cidr_blocks  = ["0.0.0.0/0"]
   }
 
   egress {
